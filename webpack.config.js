0be443d12b9826b76bebe75b7561f148e9cbfbf1
Matthew Sperry <mattjsperry@gmail.com>
2017-12-01 12:05:41 -0800

    Update dev config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,52 @@
 const path = require('path');
+const webpack = require('webpack');
+
+let plugins = []; // if using any plugins for both dev and production
+const devPlugins = []; // if using any plugins for development
+
+const prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
+
+// include plugins config
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/extry.jsx",
+  output: {
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
+  },
+  plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
 
 module.exports = {
   entry: './frontend/entry.jsx',
