2f7a31edadf0c2c57a3d6b0c34b433744b0643a9
Matthew Sperry <mattjsperry@gmail.com>
2017-12-01 14:01:51 -0800

    Update footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
 
-const handleClick = e => {
-  e.preventDefault();
-};
-
 const Footer = () => (
   <footer className="footer">
     <ul className="links">
-      <li><a href="#" onClick={handleClick}>About Genius</a></li>
-      <li><a href="#" onClick={handleClick}>Contributor Guidelines</a></li>
-      <li><a href="#" onClick={handleClick}>Press</a></li>
-      <li><a href="#" onClick={handleClick}>News Genius</a></li>
-      <li><a href="#" onClick={handleClick}>Annotate the Web</a></li>
-      <li><a href="#" onClick={handleClick}>Advertise</a></li>
-      <li><a href="#" onClick={handleClick}>Event Space</a></li>
-      <li><a href="#" onClick={handleClick}>Privacy Policy</a></li>
-      <li><a href="#" onClick={handleClick}>Licensing</a></li>
-      <li><a href="#" onClick={handleClick}>Jobs</a></li>
-      <li><a href="#" onClick={handleClick}>Developers</a></li>
-      <li><a href="#" onClick={handleClick}>Terms of Use</a></li>
-      <li><a href="#" onClick={handleClick}>Copyright Policy</a></li>
-      <li><a href="#" onClick={handleClick}>Contact us</a></li>
+      <li><a href="https://github.com/sperrow/nineties-genius" target="_blank">About Genius</a></li>
+      <li><a href="https://github.com/sperrow/nineties-genius" target="_blank">Contributor Guidelines</a></li>
+      <li><a href="https://github.com/sperrow/nineties-genius" target="_blank">Press</a></li>
+      <li><a href="https://github.com/sperrow/nineties-genius" target="_blank">News Genius</a></li>
+      <li><a href="https://github.com/sperrow/nineties-genius" target="_blank">Annotate the Web</a></li>
+      <li><a href="https://github.com/sperrow/nineties-genius" target="_blank">Advertise</a></li>
+      <li><a href="https://github.com/sperrow/nineties-genius" target="_blank">Event Space</a></li>
+      <li><a href="https://github.com/sperrow/nineties-genius" target="_blank">Privacy Policy</a></li>
+      <li><a href="https://github.com/sperrow/nineties-genius" target="_blank">Licensing</a></li>
+      <li><a href="https://github.com/sperrow/nineties-genius" target="_blank">Jobs</a></li>
+      <li><a href="https://github.com/sperrow/nineties-genius" target="_blank">Developers</a></li>
+      <li><a href="https://github.com/sperrow/nineties-genius" target="_blank">Terms of Use</a></li>
+      <li><a href="https://github.com/sperrow/nineties-genius" target="_blank">Copyright Policy</a></li>
+      <li><a href="https://github.com/sperrow/nineties-genius" target="_blank">Contact us</a></li>
     </ul>
-    <p className="copyright">© 2017 Matthew Sperry</p>
+    <p className="copyright">
+      © 2017 Matthew Sperry
+    </p>
   </footer>
 );
 
