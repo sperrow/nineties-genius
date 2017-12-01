1397e3948897e0c9ef714f97874dcce1b1d28aed
Matthew Sperry <mattjsperry@gmail.com>
2017-12-01 14:41:31 -0800

    Remove functions set on window

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-import { fetchTrack, fetchTracks } from './actions/track_actions';
-
 document.addEventListener('DOMContentLoaded', () => {
   let store;
   if (window.currentUser) {
@@ -14,11 +12,6 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  // window.fetchTrack = fetchTrack;
-  window.fetchTracks = fetchTracks;
 
   const rootEl = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, rootEl);
